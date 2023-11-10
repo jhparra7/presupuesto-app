18b7ce927663009d3ffddc0124bee61c87b4752e
Jesus Parra <parra.jesus@outlook.com>
2023-11-10 16:03:49 -0400

    first commit

---
+++ src/app/egreso/egreso.component.ts
@@ -0,0 +1,28 @@
+import { Component, Input } from '@angular/core';
+import { Egreso } from './egreso.model';
+import { EgresoServicio } from './egreso.servicio';
+
+@Component({
+  selector: 'app-egreso',
+  templateUrl: './egreso.component.html',
+  styleUrls: ['./egreso.component.css']
+})
+export class EgresoComponent {
+
+  egresos:Egreso[]=[];
+  @Input() ingresoTotal!:number;
+
+  constructor(private egresosServicio:EgresoServicio){}
+
+  ngOnInit(){
+    this.egresos=this.egresosServicio.egresos;
+  }
+
+  eliminarEgreso(egreso:Egreso){
+      this.egresosServicio.eliminar(egreso);
+  }
+
+  calcularPorcentaje(egreso:Egreso){
+    return egreso.valor / this.ingresoTotal;
+  }
+}
---
+++ src/app/ingreso/ingreso.servicio.ts
@@ -0,0 +1,15 @@
+import { Ingreso } from "./ingreso.model";
+
+export class IngresoServicio {
+
+    ingresos : Ingreso[]= [
+        new Ingreso("Salario",4000),
+        new Ingreso("Venta de Carro",500),
+    ];
+
+eliminar(ingreso:Ingreso) {
+    const indice = this.ingresos.indexOf(ingreso);
+    this.ingresos.splice(indice,1);
+}
+
+}
